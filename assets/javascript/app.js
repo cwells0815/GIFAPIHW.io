44379be80ac1b4f5de1e7c3a53d95febf58c7742
colin gaming <n.wells3@icloud.com>
2018-06-29 00:27:43 -0400

    fix https

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,7 +19,7 @@ function populateButtons(searchArray,classToAdd,areaToAddTo){
 $(document).on('click','.searchButton',function(){
     $('#searches').empty();
     var type = $(this).data('type');
-    var queryURL= 'http://api.giphy.com/v1/gifs/search?q='+type+'&api_key=FajAvAsHcAF1FDVjdyQXkFQxYyDXAYld&limit=12';
+    var queryURL= 'https://api.giphy.com/v1/gifs/search?q='+type+'&api_key=FajAvAsHcAF1FDVjdyQXkFQxYyDXAYld&limit=12';
     $.ajax({url:queryURL,method:'GET'})
     .done(function(response){
         for (var i=0;i<response.data.length;i++){
